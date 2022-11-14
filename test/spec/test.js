209490a4739da4104580349bc279a83f4cbcb976
MrSh1r0 <tungipod2708@gmail.com>
2022-11-14 18:35:12 +0100

    change file

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,1 @@
-(function () {
-  'use strict';
-
-  suite('Give it some context', function () {
-    suite('maybe a bit more context here', function () {
-      test('should run here few assertions', function () {
-
-      });
-    });
-  });
-})();
+console.log("'Allo 'Allo!");
